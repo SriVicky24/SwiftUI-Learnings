19f9b578e7f0b55ce637b841a4ad1d9ef7b3ba75
Sri Vignesh <SBadrinath@spacetalkwatch.com>
2021-10-20 12:16:18 +1100

    Initial Commit for Chat app

---
+++ ChatApp/ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Sri Badrinath on 20/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
